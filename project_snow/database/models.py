55dbd681b971fae838f2be3d8e4fe20f1237a118
daniel daum <daniel_daum@outlook.com>
2023-01-21 11:40:02 -0600

    added hourly forecast table

--- project_snow/database/models.py
+++ project_snow/database/models.py
@@ -37,3 +37,27 @@ class Daily_forecast(Base):
     valid_to = Column(TIMESTAMP(timezone=True))
 
 
+
+class Hourly_forecast(Base):
+    __tablename__ = "hourly_forecast"
+
+    id = Column(Integer, primary_key=True, nullable=False)
+    resort_id = Column(Integer, ForeignKey("resorts.id"), nullable=False)
+    period = Column(Integer, nullable=False)
+    relative_date = Column(String(50))
+    start_time = Column(Date, nullable=False)
+    end_time = Column(Date, nullable=False)
+    is_daytime = Column(Boolean)
+    temperature = Column(Integer)
+    temperature_unit = Column(String(1))
+    temperature_trend = Column(String(255))
+    wind_speed = Column(String(100))
+    wind_direction = Column(String(5))
+    icon = Column(String)
+    short_forecast = Column(String(50))
+    detailed_forecast = Column(String(100))
+
+    last_updated_at = Column(TIMESTAMP(timezone=True))
+    valid_from = Column(TIMESTAMP(timezone=True))
+    valid_to = Column(TIMESTAMP(timezone=True))
+
