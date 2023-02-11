9d33cc7d892912d9bd3ad4a49f7d378f262460f5
daniel daum <daniel_daum@outlook.com>
2023-02-11 13:31:10 -0600

    streamline env vars

--- project_snow/settings.py
+++ project_snow/settings.py
@@ -7,15 +7,8 @@ load_dotenv("./.env")
 
 
 class Settings(BaseSettings):
-    DBTYPE: str = os.getenv("DBTYPE")
-    DBUSER: str = os.getenv("DBUSER")
-    DBPASS: str = os.getenv("DBPASS")
-    DBHOST: str = os.getenv("DBHOST")
-    DBPORT: str = os.getenv("DBPORT")
-    DBNAME: str = os.getenv("DBNAME")
-    # DBNAME_TEST: str = os.getenv("DBNAME_TEST")
-    DBSTR: str = f"{DBTYPE}://{DBUSER}:{DBPASS}@{DBHOST}:{DBPORT}/{DBNAME}"
-    # DBSTR_TEST: str = f"{DBTYPE}://{DBUSER}:{DBPASS}@{DBHOST}:{DBPORT}/{DBNAME_TEST}"
+
+    DBSTR: str =os.getenv("DBSTR")
 
     KEY: str = os.getenv("KEY")
     ALGO: str = os.getenv("ALGO")
